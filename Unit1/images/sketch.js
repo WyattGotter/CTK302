8d854c5b52e1aa481488c6dbb8b0061fd7cbba0d
WyattGotter <75274320+WyattGotter@users.noreply.github.com>
2022-01-27 09:31:07 -0600

    Add files via upload

---
+++ Unit1/images/sketch.js
@@ -0,0 +1,20 @@
+let ballin;
+let bruh;
+let thebible;
+
+function setup() {
+  createCanvas(400, 400);
+
+  //how to do it: file name without extension, = loadImage(assets/filename.file extension)
+    ballin = loadImage("assets/ballin.jpg");
+    bruh = loadImage("assets/bruh.jpg");
+    thebible = loadImage("assets/thebible.jpg");
+}
+
+  //The code is (x, y, width, height)
+function draw() {
+  background(100);
+  image(ballin, width/2, 100, 100, 100);
+  image(bruh, width/2, 200, 100, 100);
+  image(thebible, width/2, 300, 100, 100)
+}
