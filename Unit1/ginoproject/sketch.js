fbea97e7423244045ab38ffc1f774dc04656b8b8
WyattGotter <75274320+WyattGotter@users.noreply.github.com>
2022-01-30 16:38:45 -0600

    g

--- Unit1/ginoproject/sketch.js
+++ Unit1/ginoproject/sketch.js
@@ -1,10 +1,11 @@
 function setup() {
   createCanvas(500, 500);
   textAlign(CENTER) ;
+  vanillaice = loadImage("assets/ginoice.jpg");
 }
 
 function draw() {
-  background('green');
+  background(image(vanillaice, width/2, 500, 500);
   fill('white');
   textSize(32);
   text("Gino looks like Vanilla Ice", 250, 250) ;
