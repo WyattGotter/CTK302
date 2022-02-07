7a92b016d453a97722ce18ee373ffd4aa87624b7
WyattGotter <75274320+WyattGotter@users.noreply.github.com>
2022-02-06 23:00:04 -0600

    e

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -1,8 +1,3 @@
-//let song1;
-
-//function preload() {
-//  song1 = loadSound("assets/beachbottoms.mp3")
-//}
 
 function setup() {
   createCanvas(400, 400);
@@ -14,7 +9,4 @@ function draw() {
   background(220);
   arc(width/2, height/2, 200, 200, 215, 135, PIE);
   fill('yellow');
-//if (mouseIsPressed)
-  //background('red');
-
 }
