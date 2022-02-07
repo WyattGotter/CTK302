050d18f67aed876c20c4c8ac92ab2b22d3ea3a83
WyattGotter <75274320+WyattGotter@users.noreply.github.com>
2022-02-06 22:51:42 -0600

    c

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -1,20 +1,20 @@
-let song1;
+//let song1;
 
-function preload() {
-  song1 = loadSound("assets/beachbottoms.mp3")
-}
+//function preload() {
+//  song1 = loadSound("assets/beachbottoms.mp3")
+//}
 
 function setup() {
   createCanvas(400, 400);
   angleMode(DEGREES);
-  song1.loop();
+  //song1.loop();
 }
 
 function draw() {
   background(220);
   arc(width/2, height/2, 200, 200, 215, 135, PIE);
   fill('yellow');
-if (mouseIsPressed)
+//if (mouseIsPressed)
   background('red');
 
 }
