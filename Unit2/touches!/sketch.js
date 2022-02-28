1758508ec9cdf96d98d4651991805e52ed73b172
WyattGotter <75274320+WyattGotter@users.noreply.github.com>
2022-02-28 11:59:57 -0600

    Update sketch.js

--- Unit2/touches!/sketch.js
+++ Unit2/touches!/sketch.js
@@ -10,31 +10,33 @@ function draw() {
   text(numberOfTouches + ' touches', 5, 10);
 
   switch (numberOfTouches) {
+
     case 0:
-      fill('black');
+
       text("no one is touching the screen", 5, 22);
-      fill('blue');
+
       break;
 
     case 1:
-      fill('black');
+
       text("it's kind of lonely here", 5, 22);
-      fill('red');
+
       // put a picture here
 
       break;
 
     case 2:
-      fill('black');
+
       text("two fingers are touching the screen", 5, 22);
-      fill('green');
+
       // put a picture here
+
       break;
 
     case 3:
-      fill('black');
+    
       text("three are touching the screen", 5, 22);
-      fill('grey')
+
       // put a picture here
       break;
 
